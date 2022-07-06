6fb7204a09a52edcc514707eb52b62ee98a8bee3
JoaoPauloDurante <joaopaulojphwa@gmail.com>
2022-07-06 18:54:45 -0300

    terminei os desafio do 1 ao 3

---
+++ desafio01.ts
@@ -0,0 +1,10 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+//let employee = {};
+//employee.code = 10;
+//employee.name = "John";
+
+const employee: { name: string, code: number}={
+    name:'Joao',
+    code: 13
+}
